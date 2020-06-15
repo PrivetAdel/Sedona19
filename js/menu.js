2b356047a5520cb6b9eaf78e31830c68310dd196
Adel <rizvanovaadel@gmail.com>
2020-06-16 00:01:41 +0300

    Updates

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,11 @@
-  var navMain = document.querySelector(".main-nav");
-  var navToggle = document.querySelector(".main-nav__toggle");
-  navMain.classList.remove("main-nav--nojs");
-  navToggle.addEventListener("click", function() {
-    if (navMain.classList.contains("main-nav--closed")) {
-      navMain.classList.remove("main-nav--closed");
-      navMain.classList.add("main-nav--opened");
-    } else {
-      navMain.classList.add("main-nav--closed");
-      navMain.classList.remove("main-nav--opened");
-    }
-  });
+var navMain = document.querySelector(".main-nav");
+var navToggle = document.querySelector(".main-nav__toggle");
+
+navMain.classList.remove("main-nav--nojs");
+navMain.classList.add("main-nav--closed");
+navMain.classList.remove("main-nav--opened");
+
+navToggle.addEventListener("click", function() {
+  navMain.classList.toggle("main-nav--closed");
+  navMain.classList.toggle("main-nav--opened");
+});
